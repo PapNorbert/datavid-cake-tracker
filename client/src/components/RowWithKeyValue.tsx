423e7128fa43cdb6a4fa9cb3928853cef48f72d9
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-06-14 19:06:30 +0300

    Editing and deleting members

--- client/src/components/RowWithKeyValue.tsx
+++ client/src/components/RowWithKeyValue.tsx
@@ -1,18 +1,43 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, FloatingLabel, Form, Row } from "react-bootstrap";
+
+import { UpdateForm } from "./members/MemberDetailedElement";
 
 interface PropType {
   keyString: string;
   valueString: string;
+  editing: boolean;
+  form: UpdateForm;
+  setFieldUpdateForm: (field: string, value: string) => void;
+  errors: UpdateForm;
+  fieldName: string
 }
 
-export default function RowWithKeyValue({ keyString, valueString }: PropType) {
+export default function RowWithKeyValue({
+  keyString, valueString, editing, form, setFieldUpdateForm, errors, fieldName
+}: PropType) {
+
+
+
   return (
     <Row>
       <Col lg='2'>
         {keyString}
       </Col>
       <Col>
-        {valueString}
+        {
+          editing ?
+            <FloatingLabel controlId='floatingCountryInput'
+              label={keyString} className='mb-3' >
+              <Form.Control type='text' placeholder={keyString}
+                value={form[fieldName]} isInvalid={!!errors[fieldName]} autoComplete='off'
+                onChange={e => { setFieldUpdateForm(fieldName, e.target.value) }} />
+              <Form.Control.Feedback type='invalid'>
+                {errors[fieldName]}
+              </Form.Control.Feedback>
+            </FloatingLabel>
+            :
+            valueString
+        }
       </Col>
     </Row>
   )
